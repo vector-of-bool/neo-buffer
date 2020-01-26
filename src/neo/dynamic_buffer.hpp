b2948cb9a19c93739a922663caa685e51ba82876
vector-of-bool <vectorofbool@gmail.com>
2020-01-25 22:56:33 -0700

    Missing typename requirements on dynamic buffer types

--- src/neo/dynamic_buffer.hpp
+++ src/neo/dynamic_buffer.hpp
@@ -20,7 +20,7 @@ struct proto_dynamic_buffer {
     std::size_t max_size() const;
     std::size_t capacity() const;
 
-    const_buffers_type data(std::size_t, std::size_t) const;
+    const_buffers_type   data(std::size_t, std::size_t) const;
     mutable_buffers_type data(std::size_t, std::size_t);
 
     mutable_buffers_type grow(std::size_t);
@@ -36,6 +36,8 @@ concept dynamic_buffer = requires(DynBuf buf,
                                   const DynBuf cbuf,
                                   std::size_t position,
                                   std::size_t size) {
+    typename DynBuf::const_buffers_type;
+    typename DynBuf::mutable_buffers_type;
     requires const_buffer_sequence<typename DynBuf::const_buffers_type>;
     requires mutable_buffer_sequence<typename DynBuf::mutable_buffers_type>;
     { cbuf.size() } -> same_as<std::size_t>;
