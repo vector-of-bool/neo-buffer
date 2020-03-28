1f6c236e2e10cc18ed21150ea45b3b45f7e684bc
vector-of-bool <vectorofbool@gmail.com>
2020-03-28 14:41:04 -0600

    static_buffer_vector for consuming small dynamically sized buffer arrays

---
+++ src/neo/static_buffer_vector.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "./buffer_concepts.hpp"
+
+#include <cassert>
+#include <cstddef>
+
+namespace neo {
+
+/**
+ * A `static_buffer_vector` represents a dynamically-sized array of buffers with
+ * a fixed maximum size. Buffers can be pushed onto the array up to a limit.
+ * This models `const_buffer_sequence` and conditionally
+ * `mutable_buffer_sequence` (if `BufferType` is mutable_buffer).
+ */
+template <typename BufferType, std::size_t MaxBuffers>
+struct static_buffer_vector {
+    using value_type  = BufferType;
+    using buffer_type = value_type;
+
+    using pointer         = buffer_type*;
+    using const_pointer   = const buffer_type*;
+    using reference       = buffer_type&;
+    using const_reference = const buffer_type&;
+    using iterator        = pointer;
+    using const_iterator  = const_pointer;
+
+    std::size_t active_count = 0;
+    buffer_type buffers[MaxBuffers == 0 ? 1 : MaxBuffers];
+
+    constexpr std::size_t size() const noexcept { return active_count; }
+    constexpr std::size_t max_size() const noexcept { return MaxBuffers; }
+
+    constexpr const_iterator cbegin() const noexcept { return buffers; }
+    constexpr const_iterator begin() const noexcept { return cbegin(); }
+    constexpr iterator       begin() noexcept { return buffers; }
+    constexpr const_iterator cend() const noexcept { return cbegin() + active_count; }
+    constexpr const_iterator end() const noexcept { return cend(); }
+    constexpr iterator       end() noexcept { return begin() + active_count; }
+
+    constexpr reference push_back(buffer_type b) noexcept {
+        assert(size() < max_size() && "static_buffer_vector::push_back() pushed too many elements");
+        auto& ret = buffers[size()] = b;
+        ++active_count;
+        return ret;
+    }
+
+    constexpr reference operator[](std::size_t idx) noexcept {
+        assert(idx < size());
+        return buffers[idx];
+    }
+
+    constexpr const_reference operator[](std::size_t idx) const noexcept {
+        assert(idx < size());
+        return buffers[idx];
+    }
+};
+
+static_assert(const_buffer_sequence<static_buffer_vector<const_buffer, 4>>);
+static_assert(mutable_buffer_sequence<static_buffer_vector<mutable_buffer, 4>>);
+
+}  // namespace neo
---
+++ src/neo/static_buffer_vector.test.cpp
@@ -0,0 +1,32 @@
+#include <neo/static_buffer_vector.hpp>
+
+#include <neo/buffer_algorithm.hpp>
+#include <neo/const_buffer.hpp>
+
+#include <catch2/catch.hpp>
+
+using namespace neo;
+
+TEST_CASE("Create a zero-length buffer vector") {
+    static_buffer_vector<const_buffer, 0> b;
+    CHECK(buffer_count(b) == 0);
+    CHECK(b.size() == 0);
+    CHECK(b.max_size() == 0);
+}
+
+TEST_CASE("Simple buffer vector") {
+    static_buffer_vector<const_buffer, 2> b;
+    CHECK(b.size() == 0);
+    CHECK(b.max_size() == 2);
+    // Push one
+    b.push_back(const_buffer("Simple string"));
+    CHECK(b.size() == 1);
+    CHECK(b[0].size() == 13);
+    CHECK(std::distance(b.begin(), b.end()) == 1);
+    CHECK(buffer_size(b) == 13);
+    // Push another
+    b.push_back(const_buffer("Another one"));
+    CHECK(b[1].size() == 11);
+    CHECK(buffer_count(b) == 2);
+    CHECK(buffer_size(b) == 24);
+}
