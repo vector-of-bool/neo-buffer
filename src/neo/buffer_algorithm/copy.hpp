a843862fade26ef5100efbacec227b15dbb588ff
vector-of-bool <vectorofbool@gmail.com>
2020-05-16 16:03:04 -0600

    Refine low-level copy to take two pointers and a size, rather than two buffers

--- src/neo/buffer_algorithm/copy.hpp
+++ src/neo/buffer_algorithm/copy.hpp
@@ -8,7 +8,6 @@
 #include "./size.hpp"
 
 #include <algorithm>
-#include <cassert>
 #include <cstddef>
 
 namespace neo {
@@ -17,12 +16,10 @@ namespace neo {
  * Low-level buffer copier that copies from the beginning to the end.
  * `dest` and `src` must have the same size()!
  */
-constexpr void ll_buffer_copy_forward(mutable_buffer dest, const_buffer src) noexcept {
-    assert(dest.size() == src.size());
-    auto in  = src.data();
-    auto out = dest.data();
-    for (; in != src.data_end(); ++in, ++out) {
-        *out = *in;
+constexpr void
+ll_buffer_copy_forward(std::byte* dest, const std::byte* src, std::size_t s) noexcept {
+    for (; s; --s) {
+        *dest++ = *src++;
     }
 }
 
@@ -30,12 +27,12 @@ constexpr void ll_buffer_copy_forward(mutable_buffer dest, const_buffer src) noe
  * Low-level buffer copier that copies from the end to the beginning
  * `dest` and `src` must have the same size()!
  */
-constexpr void ll_buffer_copy_backward(mutable_buffer dest, const_buffer src) noexcept {
-    assert(dest.size() == src.size());
-    auto r_in  = src.data_end();
-    auto r_out = dest.data_end();
-    while (r_in != src.data()) {
-        *--r_out = *--r_in;
+constexpr void
+ll_buffer_copy_backward(std::byte* dest, const std::byte* src, std::size_t s) noexcept {
+    dest = dest + s;
+    src  = src + s;
+    for (; s; --s) {
+        *--dest = *--src;
     }
 }
 
@@ -44,9 +41,9 @@ constexpr void ll_buffer_copy_backward(mutable_buffer dest, const_buffer src) no
  * `src` are disjoin. Behavior is undefined otherwise.
  * `dest` and `src` must have the same size()!
  */
-constexpr void ll_buffer_copy_fast(mutable_buffer dest, const_buffer src) noexcept {
+constexpr void ll_buffer_copy_fast(std::byte* dest, const std::byte* src, std::size_t s) noexcept {
     /// XXX: Improve with memcpy() when we have is_constant_evaluated()
-    ll_buffer_copy_forward(dest, src);
+    ll_buffer_copy_forward(dest, src, s);
 }
 
 /**
@@ -54,16 +51,20 @@ constexpr void ll_buffer_copy_fast(mutable_buffer dest, const_buffer src) noexce
  * results in the case of overlap.
  * `dest` and `src` must have the same size()!
  */
-constexpr void buffer_copy_safe(mutable_buffer dest, const_buffer src) noexcept {
-    if (std::less<>{}(dest.data(), src.data())) {
-        ll_buffer_copy_forward(dest, src);
+constexpr void ll_buffer_copy_safe(std::byte* dest, const std::byte* src, std::size_t s) noexcept {
+    if (std::less<>{}(dest, src)) {
+        ll_buffer_copy_forward(dest, src, s);
     } else {
-        ll_buffer_copy_backward(dest, src);
+        ll_buffer_copy_backward(dest, src, s);
     }
 }
 
+// clang-format off
 template <typename T>
-concept ll_buffer_copy_fn = neo::invocable<T, mutable_buffer, const_buffer>&& trivially_copyable<T>;
+concept ll_buffer_copy_fn =
+    neo::invocable<T, std::byte*, const std::byte*, std::size_t>
+    && trivially_copyable<T>;
+// clang-format on
 
 /**
  * Copy data from the source buffer into the destination buffer, with a maximum of `max_copy`. The
@@ -78,7 +79,7 @@ buffer_copy(mutable_buffer dest, const_buffer src, std::size_t max_copy, Copy co
     // sizes and the maximum bytes we want to copy
     const auto n_to_copy = (std::min)(src.size(), (std::min)(dest.size(), max_copy));
     // Do the copy!
-    copy(dest.first(n_to_copy), src.first(n_to_copy));
+    copy(dest.data(), src.data(), n_to_copy);
     return n_to_copy;
 }
 
@@ -123,17 +124,49 @@ buffer_copy(Dest&& dest, Source&& src, std::size_t max_copy, Copy copy) noexcept
  */
 template <mutable_buffer_sequence Dest, const_buffer_sequence Source>
 constexpr std::size_t buffer_copy(Dest&& dest, Source&& src) noexcept {
-    return buffer_copy(dest, src, buffer_copy_safe);
+    return buffer_copy(dest, src, ll_buffer_copy_safe);
 }
 
 template <mutable_buffer_sequence Dest, const_buffer_sequence Source>
 constexpr std::size_t buffer_copy(Dest&& dest, Source&& src, std::size_t max_copy) noexcept {
-    return buffer_copy(dest, src, max_copy, buffer_copy_safe);
+    return buffer_copy(dest, src, max_copy, ll_buffer_copy_safe);
 }
 
 template <mutable_buffer_sequence Dest, const_buffer_sequence Source, ll_buffer_copy_fn Copy>
 constexpr auto buffer_copy(Dest&& dest, Source&& src, Copy copy) noexcept {
     return buffer_copy(dest, src, std::numeric_limits<std::size_t>::max(), copy);
 }
+
+struct buffer_copy_transform_result {
+    std::size_t bytes_written = 0;
+    std::size_t bytes_read    = 0;
+    bool        done          = false;
+
+    buffer_copy_transform_result& operator+=(const buffer_copy_transform_result& o) noexcept {
+        bytes_written += o.bytes_written;
+        bytes_read += o.bytes_read;
+        done = done || o.done;
+        return *this;
+    }
+};
+
+template <ll_buffer_copy_fn Copy
+          = decltype([](std::byte* dest, const std::byte* src, std::size_t s) {
+                ll_buffer_copy_safe(dest, src, s);
+            })>
+class buffer_copy_transformer {
+    [[no_unique_address]] Copy _ll_copy;
+
+public:
+    explicit buffer_copy_transformer() = default;
+    explicit buffer_copy_transformer(Copy c)
+        : _ll_copy(c) {}
+
+    constexpr buffer_copy_transform_result operator()(mutable_buffer dest,
+                                                      const_buffer   src) const noexcept {
+        auto n_copied = buffer_copy(dest, src, _ll_copy);
+        return {n_copied, n_copied, false};
+    }
+};
 
 }  // namespace neo
