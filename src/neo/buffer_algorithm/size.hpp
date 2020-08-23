d17364d2a3584725d737f7b31f230d521799e70d
vector-of-bool <vectorofbool@gmail.com>
2020-08-23 13:59:04 -0600

    Util algorithms to check sizes without iterating a whole range.

--- src/neo/buffer_algorithm/size.hpp
+++ src/neo/buffer_algorithm/size.hpp
@@ -21,4 +21,26 @@ constexpr std::size_t buffer_size(const Seq& seq) noexcept {
 constexpr std::size_t buffer_size(const_buffer b) noexcept { return b.size(); }
 constexpr std::size_t buffer_size(mutable_buffer b) noexcept { return b.size(); }
 
+template <buffer_range R>
+constexpr bool buffer_size_at_least(R&& r, std::size_t min) noexcept {
+    std::size_t size = 0;
+    for (const_buffer b : r) {
+        size += b.size();
+        if (size >= min) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template <buffer_range R>
+constexpr bool buffer_is_empty(R&& r) noexcept {
+    for (const_buffer b : r) {
+        if (b) {
+            return false;
+        }
+    }
+    return true;
+}
+
 }  // namespace neo
