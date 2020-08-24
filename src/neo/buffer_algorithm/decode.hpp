b80c928e9ede31916a55832c6213ca477a9dcfe4
vector-of-bool <vectorofbool@gmail.com>
2020-08-23 21:25:35 -0600

    buffer_decode generically decodes objects from buffers.

---
+++ src/neo/buffer_algorithm/decode.hpp
@@ -0,0 +1,147 @@
+#pragma once
+
+#include <neo/buffer_range.hpp>
+#include <neo/buffer_source.hpp>
+#include <neo/buffers_consumer.hpp>
+
+#include <neo/iterator_concepts.hpp>
+#include <neo/out.hpp>
+
+#ifdef __has_include
+#if __has_include(<version>)
+#include <version>
+#endif
+#endif
+
+#if __cpp_lib_ranges
+#include <ranges>
+#endif
+
+namespace neo {
+
+// clang-format off
+template <typename T>
+concept buffer_decode_result =
+    semiregular<T> &&
+    requires (T res) {
+        { res.result() } noexcept;
+        { res.has_value } -> simple_boolean;
+        { res.has_error } -> simple_boolean;
+        { res.bytes_read } -> alike<std::size_t>;
+    };
+
+template <typename T>
+concept buffer_decoder = neo::invocable<T, const_buffer>;
+
+template <buffer_decoder D>
+using buffer_decode_result_t = std::invoke_result_t<D, const_buffer>;
+
+template <buffer_decoder D>
+using buffer_decode_type_t = decltype(std::declval<buffer_decode_result_t<D>>().result());
+// clang-format on
+
+/**
+ * Base case: Decode a single buffer.
+ */
+template <buffer_decoder Dec, single_buffer B>
+constexpr decltype(auto) buffer_decode(Dec&& decode, B buf) noexcept {
+    return decode(const_buffer(buf));
+}
+
+/**
+ * Upper case: Decode from a stream/range of buffers
+ */
+template <buffer_decoder Dec, buffer_input Source>
+constexpr decltype(auto) buffer_decode(Dec&& decode, Source&& source) noexcept {
+    buffer_decode_result_t<Dec> result;
+    std::size_t                 total_read = 0;
+
+    auto&& in = make_buffer_source(source);
+
+    while (true) {
+        auto next = in.next(1024);
+        if (buffer_is_empty(next)) {
+            break;
+        }
+        auto partial = buffer_decode(decode, next);
+        total_read += partial.bytes_read;
+        in.consume(partial.bytes_read);
+
+        result = std::move(partial);
+        if (result.has_value || result.has_error) {
+            break;
+        }
+    }
+
+    result.bytes_read = total_read;
+    return result;
+}
+
+// clang-format off
+/**
+ * Decode the result into an output parameter.
+ */
+template <typename Source, typename Decode, typename T>
+constexpr decltype(auto) buffer_decode(Decode&& dec,
+                                       Source&& source,
+                                       neo::output<T> out) noexcept
+    requires requires(buffer_decode_type_t<Decode> val) {
+        out.put(NEO_FWD(val));
+    }
+{
+    // clang-format on
+    auto result = buffer_decode(dec, source);
+    if (result.has_value) {
+        out.put(NEO_FWD(result.result()));
+    }
+    return result;
+}
+
+/**
+ * Decode the result into an output range denoted by two iterators.
+ */
+template <buffer_input                                  Source,
+          buffer_decoder                                Decode,
+          output_iterator<buffer_decode_type_t<Decode>> Iter,
+          sentinel_for<Iter>                            Sentinel>
+constexpr decltype(auto)
+buffer_decode(Decode&& dec, Source&& source, Iter out, Sentinel stop) noexcept {
+    buffer_decode_result_t<Decode> result{};
+    std::size_t                    total_read = 0;
+
+    auto&& in = make_buffer_source(source);
+
+    while (out != stop) {
+        auto next = in.next(1024);
+        if (buffer_is_empty(next)) {
+            break;
+        }
+        auto partial_result = buffer_decode(dec, next);
+        total_read += partial_result.bytes_read;
+        in.consume(partial_result.bytes_read);
+        result = partial_result;
+        if (result.has_value) {
+            *out = std::move(result.result());
+            ++out;
+        }
+        if (result.has_error) {
+            break;
+        }
+    }
+    result.bytes_read = total_read;
+    return result;
+}
+
+#if __cpp_lib_ranges
+/**
+ * Decode results into an output range.
+ */
+template <buffer_input                                            Source,
+          buffer_decoder                                          Decode,
+          std::ranges::output_range<buffer_decode_type_t<Decode>> Out>
+constexpr decltype(auto) buffer_decode(Decode&& dec, Source&& source, Out&& out) noexcept {
+    return buffer_decode(dec, source, std::ranges::begin(out), std::ranges::end(out));
+}
+#endif
+
+}  // namespace neo
